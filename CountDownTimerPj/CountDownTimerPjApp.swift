c5476befdf1553117640d84ae6a046cb47f95789
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-05-17 10:53:27 -0500

    Initial Commit

---
+++ CountDownTimerPj/CountDownTimerPjApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownTimerPjApp.swift
+//  CountDownTimerPj
+//
+//  Created by Ashlyn Barrera on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CountDownTimerPjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
